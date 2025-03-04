c42d1a9c5818fd7c609f600e764aaecc0a386799
Danila <55054065+danila-permogorskii@users.noreply.github.com>
2025-03-04 14:56:26 +0300

    Add data service interface

---
+++ src/BonusSystem.Core/Services/Interfaces/IDataService.cs
@@ -0,0 +1,15 @@
+using BonusSystem.Core.Repositories;
+
+namespace BonusSystem.Core.Services.Interfaces;
+
+/// <summary>
+/// Service that provides access to all repositories
+/// </summary>
+public interface IDataService
+{
+    IUserRepository Users { get; }
+    ICompanyRepository Companies { get; }
+    IStoreRepository Stores { get; }
+    ITransactionRepository Transactions { get; }
+    INotificationRepository Notifications { get; }
+}
