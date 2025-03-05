7c26d9527cee25e5eebb70f7f8aa34e060eefcf3
Danila <55054065+danila-permogorskii@users.noreply.github.com>
2025-03-05 16:15:04 +0300

    Add CompanyEntity

---
+++ src/BonusSystem.Infrastructure/DataAccess/Entities/CompanyEntity.cs
@@ -0,0 +1,20 @@
+using BonusSystem.Shared.Models;
+
+namespace BonusSystem.Infrastructure.DataAccess.Entities;
+
+public class CompanyEntity
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required string ContactEmail { get; set; }
+    public string? ContactPhone { get; set; }
+    public decimal BonusBalance { get; set; }
+    public decimal OriginalBonusBalance { get; set; }
+    public CompanyStatus Status { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    
+    // Navigation properties
+    public ICollection<StoreEntity> Stores { get; set; } = new List<StoreEntity>();
+    public ICollection<BonusTransactionEntity> Transactions { get; set; } = new List<BonusTransactionEntity>();
+}
