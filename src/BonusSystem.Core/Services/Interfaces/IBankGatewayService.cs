1f564c3ff9ad2121ba599c78c98d3c2d244d9981
vsdifficult <101355829+vsdifficult@users.noreply.github.com>
2025-05-24 17:12:33 +0300

    Update

--- src/BonusSystem.Core/Services/Interfaces/IBankGatewayService.cs
+++
@@ -1,9 +0,0 @@
-using BonusSystem.Shared.Dtos;
-
-namespace BonusSystem.Core.Services.Interfaces;
-
-public interface IPaymentService
-{
-    Task<PaymentResult> CreatePaymentAsync(FiatTransactionDto transaction);
-    Task<bool> HandleWebhookAsync(string json, string signature);
-}
--- src/BonusSystem.Core/Services/Interfaces/IStatisticsExportService.cs
+++
@@ -1,7 +0,0 @@
-namespace BonusSystem.Core.Services.Interfaces;
-
-public interface IStatisticsExportService
-{
-    Task<Stream> ExportToCsvAsync<T>(T data);
-    // Task<Stream> ExportToExcelAsync<T>(T data);
-}
